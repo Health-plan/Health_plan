c108d0d12a84d8b4e133b43d921e6a26c673a97e
KDH <dh1996m@naver.com>
2021-09-02 17:29:17 +0900

    20210902_회원가입_(이메일유효성검사작성중)

---
+++ src/main/java/com/health/domain/MemberDAO.java
@@ -0,0 +1,6 @@
+package com.health.domain;
+
+public interface MemberDAO {
+	//회원가입
+	public void insertMember(MemberDTO memberDto);
+}
---
+++ src/main/java/com/health/service/HealthPlannerService.java
@@ -0,0 +1,8 @@
+package com.health.service;
+
+import com.health.domain.MemberDTO;
+
+public interface HealthPlannerService {
+	public void insertMember(MemberDTO memberDto);
+	
+}
