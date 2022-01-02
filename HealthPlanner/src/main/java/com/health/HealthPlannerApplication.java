8e30709bbe87f175b86b7adaf2ced95d46468c5b
wellchis <chisu1995@naver.com>
2022-01-02 17:21:17 +0900

    .

--- HealthPlanner/src/main/java/com/health/HealthPlannerApplication.java
+++ HealthPlanner/src/main/java/com/health/HealthPlannerApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class HealthPlannerApplication {
-
+	
+	//main메서드는 SpringApplication.run 메서드를 호출해서 웹 애플리케이션을 실행하는 역할
 	public static void main(String[] args) {
 		SpringApplication.run(HealthPlannerApplication.class, args);
 	}
